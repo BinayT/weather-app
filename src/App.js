d8a84074f764b791314277d942a766db9e2519fb
Binay Timilsina <btimilsina@protoandgo.com>
2021-12-11 11:26:40 +0100

    Reverts done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env)
+  console.log(1)
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/utils/apiUtils.js
@@ -0,0 +1,7 @@
+const API_KEY = "2a6d8d89dbf667f0ca5adec338f22c19";
+
+const baseURL = 'https://api.openweathermap.org/data/2.5/'
+
+// forecast/daily?q={city name}&cnt={cnt}&appid={API key}
+
+export const 
