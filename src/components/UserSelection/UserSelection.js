8679860b96e21735d851d2d199d2bc5f1860a82d
Binay Timilsina <btimilsina@protoandgo.com>
2021-12-13 09:36:01 +0100

    Hot fix, user selection displaying correctly

--- src/components/UserSelection/UserSelection.js
+++ src/components/UserSelection/UserSelection.js
@@ -5,11 +5,11 @@ import {capitalizeFirstLetter} from '../../helpers/capitalize'
 import './style.css'
 
 const UserSelection = ({city, timeframe}) => {
-    return (
-        <div className="userSelectionContainer">
-            <p className="userSelectionTitle">{capitalizeFirstLetter(city)}, {timeframe}</p>
-        </div>
-    )
+        return (
+            <div className="userSelectionContainer">
+                <p className="userSelectionTitle">{city && `${capitalizeFirstLetter(city)}, ${timeframe}`}</p>
+            </div>
+        )
 }
 
 export default UserSelection
